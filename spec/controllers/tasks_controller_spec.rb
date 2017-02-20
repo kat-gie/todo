76439c31107f9bb6e130e6086bb356cda319aed2
kat-gie <gielenkatie@gmail.com>
2017-02-20 16:32:59 -0600

    form submission

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,20 +2,15 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
-    it "should list the tasks in the DB" do
+    it "should list the tasks in the database" do
       task1 = FactoryGirl.create(:task)
       task2 = FactoryGirl.create(:task)
-      task1.update_attributes(title: "Something else")
       get :index
-      expect(response).to have_http_status :success
+      assert_response :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-      response_ids = response_value.each do |task|
-        task["id"]
-      end 
-      expect(response_ids).to eq([task1.id, task2.id])
-    end 
-  end 
+    end
+  end
 
   describe "tasks#update" do
     it "should allow tasks to be marked as done" do
@@ -26,4 +21,14 @@ RSpec.describe TasksController, type: :controller do
       expect(task.done).to eq(true)
     end
   end
+
+  describe "task#create" do
+    it "should allow new tasks to be created" do 
+      post :create, task: {title: "Fix things"}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
+    end 
+  end 
 end
